4115f33868faefc7a37f9c6713ab745721ef6af1
kcenzlie <JHCL133@outlook.com>
2025-08-05 16:15:46 +0800

    correct link

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/abstract_thoughts/',
+  base: '/chinese_ver_abstract_thoughts/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
